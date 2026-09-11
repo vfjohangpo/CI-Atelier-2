10729bf3f716e3912ed04c71f4a671a30d9e9764
vfjohangpo <vfjohan.gpo@gmail.com>
2026-09-11 10:32:41 +0200

    CI : Test avec  retry automatique

---
+++ tests/test_retry.py
@@ -0,0 +1,9 @@
+import os
+
+def test_retry():
+    attempt = os.getenv("CI_RETRY_TEST")
+
+    if attempt == "fail":
+        raise AssertionError("Échec volontaire pour tester le retry")
+
+    assert True
