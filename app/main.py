056126b7a49e103a8d63823cb30c93764d9284dc
vfjohangpo <vfjohan.gpo@gmail.com>
2026-09-11 09:14:48 +0200

    Test CI

--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ app.include_router(health.router)
 app.include_router(version.router)
 app.include_router(items.router, prefix="/items", tags=["items"])
 
-#Ceci est un message de test pour le commit
+# Ceci est un message de test pour le commit
